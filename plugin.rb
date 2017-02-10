9ac8b478e7221bb2b5f7e7629d973c5754e3ebcf
Joe Buhlig <joe@joebuhlig.com>
2017-02-10 07:12:19 -0600

    Validate license key in settings

--- plugin.rb
+++ plugin.rb
@@ -24,3 +24,27 @@ Discourse::Application.routes.append do
 end
 
 load File.expand_path('../lib/discourse_league/engine.rb', __FILE__)
+
+require 'open-uri'
+require 'net/http'
+
+DiscourseEvent.on(:site_setting_saved) do |site_setting|
+  if site_setting.name.to_s == "league_license_key" && site_setting.value_changed?
+    validate_url = "https://discourseleague.com/licenses/validate?id=39973&key=" + site_setting.value
+    request = Net::HTTP.get(URI.parse(validate_url))
+    result = JSON.parse(request)
+    
+    if result["errors"]
+      raise Discourse::InvalidParameters.new(
+        'Sorry. That key is invalid.'
+      )
+    end
+
+    if result["enabled"]
+      SiteSetting.league_licensed = true
+    else
+      SiteSetting.league_licensed = false
+    end
+
+  end
+end
