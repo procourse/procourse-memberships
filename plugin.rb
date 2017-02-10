d1e80180c7f28f364c1857593389892788c636ae
Joe Buhlig <joe@joebuhlig.com>
2017-02-10 07:24:55 -0600

    Allow empty license key setting

--- plugin.rb
+++ plugin.rb
@@ -30,20 +30,25 @@ require 'net/http'
 
 DiscourseEvent.on(:site_setting_saved) do |site_setting|
   if site_setting.name.to_s == "league_license_key" && site_setting.value_changed?
-    validate_url = "https://discourseleague.com/licenses/validate?id=39973&key=" + site_setting.value
-    request = Net::HTTP.get(URI.parse(validate_url))
-    result = JSON.parse(request)
-    
-    if result["errors"]
-      raise Discourse::InvalidParameters.new(
-        'Sorry. That key is invalid.'
-      )
-    end
 
-    if result["enabled"]
-      SiteSetting.league_licensed = true
-    else
+    if site_setting.value.empty?
       SiteSetting.league_licensed = false
+    else
+      validate_url = "https://discourseleague.com/licenses/validate?id=39973&key=" + site_setting.value
+      request = Net::HTTP.get(URI.parse(validate_url))
+      result = JSON.parse(request)
+      
+      if result["errors"]
+        raise Discourse::InvalidParameters.new(
+          'Sorry. That key is invalid.'
+        )
+      end
+
+      if result["enabled"]
+        SiteSetting.league_licensed = true
+      else
+        SiteSetting.league_licensed = false
+      end
     end
 
   end
