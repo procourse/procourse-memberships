4675068d37bb06f8d2e46fe11b54d36dac358a04
Justin DiRose <justin@justindirose.com>
2018-11-02 20:14:23 -0500

    FIX: Success state checking

--- lib/procourse_memberships/engine.rb
+++ lib/procourse_memberships/engine.rb
@@ -37,7 +37,7 @@ module ProcourseMemberships
             gateway = ProcourseMemberships::Billing::Gateways.new.gateway
             response = gateway.unsubscribe(JSON.parse(subscription.value)[0]["subscription_id"])
 
-            if response.success? && user
+            if (response.success? || response[:response][:success] == true) && user
 
               memberships_gateway = ProcourseMemberships::Billing::Gateways.new(:user_id => user_id, :product_id => JSON.parse(subscription.value)[0]["product_id"])
               memberships_gateway.unstore_subscription
